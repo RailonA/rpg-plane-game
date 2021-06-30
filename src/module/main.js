df69f45f9e82a45886734ab471f84508ba01502c
RailonA <railonacosta@gmail.com>
2021-06-30 12:56:53 -0400

    Add phaser Template

--- src/module/main.js
+++ src/module/main.js
@@ -1,11 +1,26 @@
 import Phaser from 'phaser';
+import config from '../config/config';
+import gameScene from '../scenes/gameScene';
+import bootScene from '../scenes/bootScene';
+import preloaderScene from '../scenes/preloaderScene';
+import titleScene from '../scenes/titleScene';
+import optionsScene from '../scenes/optionsScene';
+import creditsScene from '../scenes/creditsScene';
+import Model from '../model';
 
-import { SimpleScene } from '../scenes/simple-scene';
+class Game extends Phaser.Game {
+    constructor () {
+      super(config);
+      const model = new Model();
+      this.globals = { model, bgMusic: null };
+      this.scene.add('Boot', bootScene);
+      this.scene.add('Preloader', preloaderScene);
+      this.scene.add('Title', titleScene);
+      this.scene.add('Options', optionsScene);
+      this.scene.add('Credits', creditsScene);
+      this.scene.add('Game', gameScene);
+      this.scene.start('Boot');
+    }
+  }
 
-const gameConfig = {
-  width: 680,
-  height: 400,
-  scene: SimpleScene
-};
-
-new Phaser.Game(gameConfig);
+  window.game = new Game();
